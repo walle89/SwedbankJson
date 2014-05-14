147699b1202e6cbf2c0809448a4c7f82fca72cbf
Eric Wallmander <git@wallmander.nu>
2014-05-15 00:20:27 +0200

    Konverterad till Guzzle och uppdaterad struktur. Fixed #6

--- src/swedbankJson.php
+++ src/swedbankJson.php
@@ -12,6 +12,9 @@ namespace walle89\SwedbankJson;
 use Exception;
 use Rhumsaa\Uuid\Uuid;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Stream\Stream;
+
 /**
  * Class SwedbankJson
  */
@@ -21,26 +24,6 @@ class SwedbankJson
      * Bas-url för API-anrop
      */
     const baseUri = 'https://auth.api.swedbank.se/TDE_DAP_Portal_REST_WEB/api/v1/';
-
-    /**
-     * @var resource CURL-resurs
-     */
-    private $_ch;
-
-    /**
-     * @var string Sökväg för cookiejarl
-     */
-    private $_ckfile;
-
-    /**‚
-     * @var string Auth-nyckel mot Swedbank
-     */
-    private $_authorization;
-
-    /**
-     * @var array Gemensamma headrs i alla anrop mot API:et
-     */
-    private $_commonHttpHeaders;
 
     /**
      * @var int Inlogging personnummer
@@ -63,6 +46,21 @@ class SwedbankJson
     private $_useragent;
 
     /**
+     * @var string Auth-nyckel mot Swedbank
+     */
+    private $_authorization;
+
+    /**
+     * @var resource CURL-resurs
+     */
+    private $_client;
+
+    /**
+     * @var string Sökväg för cookiejarl
+     */
+    private $_ckfile;
+
+    /**
      * @var string Profiltyp
      */
     private $_profileType;
@@ -73,26 +71,67 @@ class SwedbankJson
      * @param int    $username      Personnummer för inlogging till internetbanken
      * @param string $password      Personlig kod för inlogging till internetbanken
      * @param array  $appdata       En array med nödvändig datar för API:et
+     * @param bool   $debug         En array med nödvändig datar för API:et
      * @param string $ckfile        Sökväg till mapp där cookiejar kan temporärt spara
      *
      * @throws $appdata om argumentet $appdata inte är av typen 'array' eller inte har rätt index och värden
      */
-    public function __construct($username, $password, $appdata, $ckfile = './temp/')
+    public function __construct($username, $password, $appdata, $debug=false, $ckfile = './temp/')
     {
         $this->_username      = $username;
         $this->_password      = $password;
         $this->setAppData($appdata);
         $this->_ckfile        = tempnam($ckfile, 'CURLCOOKIE');
         $this->setAuthorizationKey();
+        $this->_debug         = (bool)$debug;
     }
 
     /**
      * Utlogging från API:et.
+     *
      * @see self::terminate()
      */
     public function __destruct()
     {
         $this->terminate();
+    }
+
+    /**
+     * @param string $key Sätta en egen AuthorizationKey
+     */
+    public function setAuthorizationKey($key = '')
+    {
+        $this->_authorization = (empty($key)) ? $this->genAuthorizationKey() : $key;
+    }
+
+    /**
+     * Generera auth-nyckel för att kunna komunicera med Swedbanks servrar
+     *
+     * @return string en slumpad auth-nyckel
+     */
+    public function genAuthorizationKey()
+    {
+        return base64_encode($this->_appID . ':' . strtoupper(Uuid::uuid4()));
+    }
+
+    /**
+     * Inlogging
+     * Loggar in med personummer och personig kod för att få reda på bankID och den tillfälliga profil-id:t
+     *
+     * @return bool         True om inloggingen lyckades
+     * @throws Exception    Fel vid inloggen
+     */
+    private function login()
+    {
+        $data_string = json_encode(array('useEasyLogin' => false, 'password' => $this->_password, 'generateEasyLoginId' => false, 'userId' => $this->_username,));
+        $output = $this->postRequest('identification/personalcode', $data_string, true);
+
+        if ($output->generateEasyLoginId)
+            throw new Exception('Byte av personlig kod krävs av banken. Var god rätta till detta genom att logga in på internetbanken.', 11);
+        elseif (!isset($output->links->next->uri))
+            throw new Exception('Inlogging misslyckades. Kontrollera användarnman, lösenord och authorization-nyckel.', 10);
+
+        return true;
     }
 
     /**
@@ -104,7 +143,7 @@ class SwedbankJson
      */
     public function profileList()
     {
-        if (empty($ch))
+        if (empty($this->_client))
             $this->login();
 
         $output = $this->getRequest('profile/');
@@ -122,7 +161,7 @@ class SwedbankJson
             else
                 throw new Exception('Profilsidan innerhåller inga bankkonton.', 23);
         }
-        return $output->banks;
+        return $output->banks[0];
     }
 
     /**
@@ -164,9 +203,30 @@ class SwedbankJson
     }
 
     /**
+     * Väjer profil
+     *
+     * @param $profileID
+     * @throws UserException
+     * @throws \Exception
+     */
+    private function selectProfile($profileID)
+    {
+        // Om profil inte är definerad, hämta standardprofil
+        if (empty($profileID)) {
+            $profiles = $this->profileList();
+            $profileData = $profiles->{$this->_profileType}; // Väljer privat- eller företagskonto beroende på angiven useragent
+
+            $profileID = (isset($profileData->id)) ? $profileData->id : $profileData[0]->id;
+        }
+
+        // Väljer profil
+        $this->postRequest('profile/' . $profileID);
+    }
+
+    /**
      * Visar kontodetaljer och transaktioner för konto
      *
-     * @param $accoutID             string  Unika och slumpade konto-id från Swedbank API
+     * @param $accoutID             string  Unik och slumpvis konto-id från Swedbank API
      * @param $transactionsPerPage  int     Antal transaktioner som listas "per sida". Måste vara ett heltal större eller lika med 1.
      * @param $page                 int     Aktuell sida. Måste vara ett heltal större eller lika med 1. $transactionsPerPage måste anges.
      *
@@ -179,7 +239,7 @@ class SwedbankJson
         if ($transactionsPerPage > 0 AND $page >= 1)
             $query = array('transactionsPerPage' => (int)$transactionsPerPage, 'page' => (int)$page,);
 
-        $output = $this->getRequest('engagement/transactions/' . $accoutID, null, $query);
+        $output = $this->getRequest('engagement/transactions/' . $accoutID, $query);
 
         if (!isset($output->transactions))
             throw new Exception('AccountID stämmer inte', 50);
@@ -196,24 +256,6 @@ class SwedbankJson
     }
 
     /**
-     * @param string $key Sätta en egen AuthorizationKey
-     */
-    public function setAuthorizationKey($key = '')
-    {
-        $this->_authorization = (empty($key)) ? $this->genAuthorizationKey() : $key;
-    }
-
-    /**
-     * Generera auth-nyckel för att kunna komunicera med Swedbanks servrar
-     *
-     * @return string en slumpad auth-nyckel
-     */
-    public function genAuthorizationKey()
-    {
-        return base64_encode($this->_appID . ':' . strtoupper(Uuid::uuid4()));
-    }
-
-    /**
      * @param $appdata
      * @throws \Exception
      */
@@ -224,227 +266,120 @@ class SwedbankJson
 
         $this->_appID       = $appdata['appID'];
         $this->_useragent   = $appdata['useragent'];
-        $this->_profileType = (strpos($this->_useragent, 'Corporate')) ? 'corporateProfiles' : 'privateProfile';
-    }
-
-    /**
-     * Inlogging
-     * Loggar in med personummer och personig kod för att få reda på bankID och den tillfälliga profil-id:t
-     *
-     * @return bool         True om inloggingen lyckades
-     * @throws Exception    Fel vid inloggen
-     */
-    private function login()
-    {
-        $data_string = json_encode(array('useEasyLogin' => false, 'password' => $this->_password, 'generateEasyLoginId' => false, 'userId' => $this->_username,  ));
-        $output      = $this->postRequest('identification/personalcode', $data_string, true);
-
-        if($output->generateEasyLoginId)
-            throw new Exception('Byte av personlig kod krävs av banken. Var god rätta till detta genom att logga in på internetbanken.', 11);
-        elseif (!isset($output->links->next->uri))
-            throw new Exception('Inlogging misslyckades. Kontrollera användarnman, lösenord och authorization-nyckel.', 10);
-
-        return true;
-    }
-
-    /**
-     * Väjer profil
-     *
-     * @param $profileID
-     * @throws UserException
-     * @throws \Exception
-     */
-    private function selectProfile($profileID)
-    {
-        // Om profil inte är definerad, hämta standardprofil
-        if (empty($profileID))
-        {
-            $profiles = $this->profileList();
-            $profileData = $profiles->banks[0]->{$this->_profileType}; // Väljer privat- eller företagskonto beroende på angiven useragent
-
-            $profileID = (isset($profileData->id)) ? $profileData->id : $profileData[0]->id;
-        }
-
-        // Väljer profil
-        $this->postRequest('profile/' . $profileID);
+        $this->_profileType = (strpos($this->_useragent, 'Corporate')) ? 'corporateProfiles' : 'privateProfile'; // För standardprofil
     }
 
     /**
      * Skickar GET-förfrågan
      *
-     * @param string $requestType   Typ av anrop mot API:et
-     * @param string $baseURL       Bas-url, om inget anges körs self::baseUri @see self::baseUri
+     * @param string $apiRequest   Typ av anrop mot API:et
      * @param array  $query         Fråga för GET-anrop
-     * @param bool   $debug         True om debug-data ska retuneras
      *
-     * @return object    JSON-avkodad information från API:et eller debugdata om $debug är satt till true.
+     * @return object    JSON-avkodad information från API:et
      */
-    private function getRequest($requestType, $baseURL = self::baseUri, $query = array(), $debug = false)
+    private function getRequest($apiRequest, $query = array())
     {
-        if (empty($this->_ch))
-            $this->initCurl();
+        $request = $this->createRequest('get', $apiRequest, $query);
+        $response = $this->_client->send($request);
 
-        if (is_null($baseURL))
-            $baseURL = self::baseUri;
-
-        $dsid = $this->dsid();
-
-        $httpQuery = http_build_query(array_merge($query, array( 'dsid' => $dsid )));
-
-        curl_setopt($this->_ch, CURLOPT_URL, $baseURL . $requestType . '?' . $httpQuery);
-        curl_setopt($this->_ch, CURLOPT_HTTPGET, true);
-        curl_setopt($this->_ch, CURLOPT_HTTPHEADER, $this->requestHeaders($dsid));
-        if ($debug)
-        {
-            curl_setopt($this->_ch, CURLOPT_HEADER, 1);
-            curl_setopt($this->_ch, CURLINFO_HEADER_OUT, 1);
-        }
-
-        $data = curl_exec($this->_ch);
-
-        if ($debug)
-        {
-            $headers = curl_getinfo($this->_ch, CURLINFO_HEADER_OUT);
-
-            return array( 'request' => $headers, 'response' => $data );
-        }
-        else
-            return json_decode($data);
+        return $response->json(['object' => true]);
     }
 
     /**
      * Skickar POST-förfrågan
      *
-     * @param string $requestType   Typ av anrop mot API:et
-     * @param string $data_string   Data som ska skickas i strängformat och enligt http_build_query() @see http_build_query()
-     * @param bool   $debug         True om debug-data ska retuneras
+     * @param string $apiRequest    Typ av anrop mot API:et
+     * @param string $data_string   Data som ska skickas i strängformat
      *
-     * @return object    JSON-avkodad information från API:et eller debugdata om $debug är satt till true.
+     * @return object    JSON-avkodad information från API:et
      */
-    private function postRequest($requestType, $data_string = '', $debug = false)
+    private function postRequest($apiRequest, $data_string = '')
     {
-        if (empty($this->_ch))
-            $this->initCurl();
+        $request = $this->createRequest('post', $apiRequest);
 
-        $dsid = $this->dsid();
-        curl_setopt($this->_ch, CURLOPT_URL, self::baseUri . $requestType . '?dsid=' . $dsid);
-        curl_setopt($this->_ch, CURLOPT_POST, true);
-        if (!empty($data_string))
+        if(!empty($data_string))
         {
-            curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $data_string);
-            curl_setopt($this->_ch, CURLOPT_HTTPHEADER, $this->requestHeaders($dsid, $data_string));
-        }
-        else
-            curl_setopt($this->_ch, CURLOPT_HTTPHEADER, $this->requestHeaders($dsid));
-
-        if ($debug)
-        {
-            curl_setopt($this->_ch, CURLOPT_HEADER, 1);
-            curl_setopt($this->_ch, CURLINFO_HEADER_OUT, 1);
-            curl_setopt($this->_ch, CURLOPT_NOPROGRESS, 0);
+            $request->addHeader('Content-Type', 'application/json; charset=UTF-8');
+            $request->setBody(Stream::factory($data_string));
         }
 
-        $data = curl_exec($this->_ch);
-
-        if ($debug)
-        {
-            $headers = curl_getinfo($this->_ch, CURLINFO_HEADER_OUT);
-
-            return array( 'request' => $headers.$data_string, 'response' => $data );
-        }
-        else
-            return json_decode($data);
+        $response = $this->_client->send($request);
+        return $response->json(['object' => true]);
     }
 
     /**
      * Skickar PUT-förfrågan
      *
-     * @param string $requestType     Typ av anrop mot API:et
-     * @param bool   $debug           Sätt till true för att via debug-information
+     * @param string $apiRequest Typ av anrop mot API:et
      *
-     * @return object    Avkodad JSON-data från API:et eller debuginformation om $debug är satt till true.
+     * @return object    Avkodad JSON-data från API:et
      */
-    private function putRequest($requestType, $debug = false)
+    private function putRequest($apiRequest)
     {
-        if (empty($this->_ch))
-            $this->initCurl();
+        $request = $this->createRequest('put', $apiRequest);
+        $response = $this->_client->send($request);
 
-        $dsid = $this->dsid();
-        curl_setopt($this->_ch, CURLOPT_URL, self::baseUri . $requestType . '?dsid=' . $dsid);
-        curl_setopt($this->_ch, CURLOPT_PUT, true);
-        curl_setopt($this->_ch, CURLOPT_HTTPHEADER, $this->requestHeaders($dsid));
-        if ($debug)
-        {
-            curl_setopt($this->_ch, CURLOPT_HEADER, 1);
-            curl_setopt($this->_ch, CURLINFO_HEADER_OUT, 1);
-        }
-
-        $data = curl_exec($this->_ch);
-
-        if ($debug)
-        {
-            $headers = curl_getinfo($this->_ch, CURLINFO_HEADER_OUT);
-
-            return array( 'request' => $headers, 'response' => $data );
-        }
-        else
-            return json_decode($data);
+        return $response->json(['object' => true]);
     }
 
     /**
-     * Iniserar CURL och gemensamma HTTP-headers
-     */
-    private function initCurl()
-    {
-        $this->_commonHttpHeaders = array(
-            'Authorization: ' . $this->_authorization,
-            'Accept: */*',
-            'Accept-Language: sv-se',
-            'Accept-Encoding: gzip, deflate',
-            'Connection: keep-alive',
-            'Proxy-Connection: keep-alive'
-        );
-
-        $this->_ch = curl_init();
-        curl_setopt($this->_ch, CURLOPT_COOKIEJAR, $this->_ckfile);
-        curl_setopt($this->_ch, CURLOPT_COOKIEFILE, $this->_ckfile);
-        curl_setopt($this->_ch, CURLOPT_USERAGENT, $this->_useragent);
-        curl_setopt($this->_ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->_ch, CURLOPT_AUTOREFERER, true);
-        curl_setopt($this->_ch, CURLOPT_FOLLOWLOCATION, true);
-    }
-
-    /**
-     * Gemensama HTTP-headers som anapassas efter typ av anriop
+     * Gemensam hantering av HTTP requests
      *
-     * @param string $dsid          8 slumpvalda tecken @see self::dsid();
-     * @param string $data_string   Data som ska skickas. Används för att räkna ut längen på texten.
-     *
-     * @return array    HTTP:haders för att användas i CURL-anrop.
+     * @param string    $method
+     * @param string    $apiRequest Requesttyp till API
+     * @param array     $query      Ev. query till URL
+     * @return mixed    @see \GuzzleHttp\Client\createRequest
      */
-    private function requestHeaders($dsid, $data_string = '')
+    private function createRequest($method, $apiRequest, array $query=[])
     {
-        curl_setopt($this->_ch, CURLOPT_COOKIE, "dsid=$dsid");
-        $requestHeader = $this->_commonHttpHeaders;
-
-        if (!empty($data_string))
+        if (empty($this->_client))
         {
-            $requestHeader[] = 'Content-Type: application/json; charset=UTF-8';
-            $requestHeader[] = 'Content-Length: ' . strlen($data_string);
+            $this->_client = new Client([
+                'base_url' => self::baseUri,
+                'defaults' => [
+                    'headers' => [
+                        'Authorization' => $this->_authorization,
+                        'Accept' => '*/*',
+                        'Accept-Language' => 'sv-se',
+                        'Accept-Encoding' => 'gzip, deflate',
+                        'Connection' => 'keep-alive',
+                        'Proxy-Connection' => 'keep-alive',
+                        'User-Agent' => $this->_useragent,
+                    ],
+                    'allow_redirects' => ['max' => 10, 'referer' => true],
+                    'config' => [
+                        'curl' => [
+                            CURLOPT_COOKIEJAR => $this->_ckfile,
+                            CURLOPT_COOKIEFILE => $this->_ckfile,
+                        ],
+                    ],
+                ],
+                'debug' => $this->_debug,
+            ]);
         }
 
-        return $requestHeader;
+        $dsidStr = ['dsid' => $this->dsid()];
+
+        $httpQuery = array_merge($query, $dsidStr);
+
+        return $this->_client->createRequest($method, $apiRequest, [
+            'cookies'   => $dsidStr,
+            'query'     => $httpQuery,
+        ]);
     }
 
     /**
      * Generering av dsid
-     * Slumpar 8 tecken som måste skickas med i varje anrop. Antagligen för att API:et vill förhindra en cache skapas
+     * Slumpar 8 tecken som måste skickas med i varje anrop.
      *
      * @return string   8 slumpvalda tecken
      */
     private function dsid()
     {
+        // Välj 8 tecken
         $dsid = substr(sha1(mt_rand()), rand(1, 30), 8);
+
+        // Gör 4 tecken till versaler
         $dsid = substr($dsid, 0, 4) . strtoupper(substr($dsid, 4, 4));
 
         return str_shuffle($dsid);
