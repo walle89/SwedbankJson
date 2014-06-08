a8cf424819d5249758f247d8ec4dc58e88c7b32d
Eric Wallmander <git@wallmander.nu>
2014-06-08 13:58:43 +0200

    Stöd för Sparbanken Företag

--- src/AppData.php
+++ src/AppData.php
@@ -15,11 +15,12 @@ namespace SwedbankJson;
 class AppData
 {
     private static $appData = array(
-        'swedbank'          => array('appID' => 'fjUHYbQjupOQspeG', 'useragent' => 'SwedbankMOBPrivateIOS/3.7.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
-        'sparbanken'        => array('appID' => 'WXessEsk4s1ZE4GO', 'useragent' => 'SavingbankMOBPrivateIOS/3.7.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
-        'swedbank_ung'      => array('appID' => 'XlqIpRxVTTgHuFnj', 'useragent' => 'SwedbankMOBYouthIOS/1.4.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
-        'sparbanken_ung'    => array('appID' => 'XOn0HeGwPmELslnO', 'useragent' => 'SavingbankMOBYouthIOS/1.4.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
-        'swedbank_företag'  => array('appID' => '1e7ZaX5kq3Dk4mCK', 'useragent' => 'SwedbankMOBCorporateIOS/1.4.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'swedbank'              => array('appID' => 'fjUHYbQjupOQspeG', 'useragent' => 'SwedbankMOBPrivateIOS/3.7.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'sparbanken'            => array('appID' => 'WXessEsk4s1ZE4GO', 'useragent' => 'SavingbankMOBPrivateIOS/3.7.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'swedbank_ung'          => array('appID' => 'XlqIpRxVTTgHuFnj', 'useragent' => 'SwedbankMOBYouthIOS/1.4.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'sparbanken_ung'        => array('appID' => 'XOn0HeGwPmELslnO', 'useragent' => 'SavingbankMOBYouthIOS/1.4.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'swedbank_företag'      => array('appID' => '1e7ZaX5kq3Dk4mCK', 'useragent' => 'SwedbankMOBCorporateIOS/1.4.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'sparbanken_foretag'    => array('appID' => 'tuLer1xBdoQChAEM', 'useragent' => 'SavingbankMOBCorporateIOS/1.4.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
     );
 
     public static function bankAppId($bankID)
