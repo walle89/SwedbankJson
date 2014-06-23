91eea1491b717ea0b69d0a300bfb2babaf2476e5
Eric Wallmander <git@wallmander.nu>
2014-06-23 23:17:20 +0200

    Uppdatering av appversion (Swedbank och Sparbanken).

--- src/AppData.php
+++ src/AppData.php
@@ -15,8 +15,8 @@ namespace SwedbankJson;
 class AppData
 {
     private static $appData = array(
-        'swedbank'              => array('appID' => 'mPXoKcA3jsKP0cAQ', 'useragent' => 'SwedbankMOBPrivateIOS/3.8.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
-        'sparbanken'            => array('appID' => 'q5JD05YQBoaMuzR2', 'useragent' => 'SavingbankMOBPrivateIOS/3.8.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'swedbank'              => array('appID' => 'tpEx4x9jN1SKDbzr', 'useragent' => 'SwedbankMOBPrivateIOS/3.8.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
+        'sparbanken'            => array('appID' => 'FzaohcXUyIAfanJ8', 'useragent' => 'SavingbankMOBPrivateIOS/3.8.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
         'swedbank_ung'          => array('appID' => 'ZvBWfugmcLdNo2r2', 'useragent' => 'SwedbankMOBYouthIOS/1.5.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
         'sparbanken_ung'        => array('appID' => 'VNxDH9rfpWrCDzsL', 'useragent' => 'SavingbankMOBYouthIOS/1.5.0_(iOS;_7.1.1)_Apple/iPhone5,2'),
         'swedbank_foretag'      => array('appID' => '1e7ZaX5kq3Dk4mCK', 'useragent' => 'SwedbankMOBCorporateIOS/1.4.1_(iOS;_7.1.1)_Apple/iPhone5,2'),
