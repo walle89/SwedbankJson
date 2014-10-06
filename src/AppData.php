b72a30c9c67486363c0348410e40048242d4b24a
Eric Wallmander <git@wallmander.nu>
2014-10-06 21:55:39 +0200

    Små justeringar

--- src/AppData.php
+++ src/AppData.php
@@ -14,22 +14,23 @@ namespace SwedbankJson;
  */
 class AppData
 {
-    private static $appData = array(
-        'swedbank'              => array('appID' => 'HithYAGrzi8fu73j', 'useragent' => 'SwedbankMOBPrivateIOS/3.9.0_(iOS;_8.0.2)_Apple/iPhone5,2'),
-        'sparbanken'            => array('appID' => '9iZSu74jfDFaTdPd', 'useragent' => 'SavingbankMOBPrivateIOS/3.9.0_(iOS;_8.0.2)_Apple/iPhone5,2'),
-        'swedbank_ung'          => array('appID' => 'IV4Wrt2VZtyYjfpW', 'useragent' => 'SwedbankMOBYouthIOS/1.6.0_(iOS;_8.0.2)_Apple/iPhone5,2'),
-        'sparbanken_ung'        => array('appID' => 'BrGkZQR89rEbFwnj', 'useragent' => 'SavingbankMOBYouthIOS/1.6.0_(iOS;_8.0.2)_Apple/iPhone5,2'),
-        'swedbank_foretag'      => array('appID' => 'v0RVbFGKMXz7U4Eb', 'useragent' => 'SwedbankMOBCorporateIOS/1.5.0_(iOS;_8.0.2)_Apple/iPhone5,2'),
-        'sparbanken_foretag'    => array('appID' => 'JPf1VxiskNdFSclr', 'useragent' => 'SavingbankMOBCorporateIOS/1.5.0_(iOS;_8.0.2)_Apple/iPhone5,2'),
-    );
+    private static $appData = [
+        'swedbank'              => [ 'appID' => 'HithYAGrzi8fu73j', 'useragent' => 'SwedbankMOBPrivateIOS/3.9.0_(iOS;_8.0.2)_Apple/iPhone5,2'       ],
+        'sparbanken'            => [ 'appID' => '9iZSu74jfDFaTdPd', 'useragent' => 'SavingbankMOBPrivateIOS/3.9.0_(iOS;_8.0.2)_Apple/iPhone5,2'     ],
+        'swedbank_ung'          => [ 'appID' => 'IV4Wrt2VZtyYjfpW', 'useragent' => 'SwedbankMOBYouthIOS/1.6.0_(iOS;_8.0.2)_Apple/iPhone5,2'         ],
+        'sparbanken_ung'        => [ 'appID' => 'BrGkZQR89rEbFwnj', 'useragent' => 'SavingbankMOBYouthIOS/1.6.0_(iOS;_8.0.2)_Apple/iPhone5,2'       ],
+        'swedbank_foretag'      => [ 'appID' => 'v0RVbFGKMXz7U4Eb', 'useragent' => 'SwedbankMOBCorporateIOS/1.5.0_(iOS;_8.0.2)_Apple/iPhone5,2'     ],
+        'sparbanken_foretag'    => [ 'appID' => 'JPf1VxiskNdFSclr', 'useragent' => 'SavingbankMOBCorporateIOS/1.5.0_(iOS;_8.0.2)_Apple/iPhone5,2'   ],
+    ];
 
     public static function bankAppId($bankID)
     {
-        if($bankID == 'swedbank_företag')
+        if ($bankID == 'swedbank_företag')
             throw new UserException('Bankid "swedbank_företag" är inte längre giltigt. Använd "swedbank_foretag"',1);
-        elseif(!array_key_exists($bankID, self::$appData))
+
+        elseif (!isset(self::$appData[ $bankID ]))
             throw new UserException('BankID existerar inte, använd något av följande: '.implode(', ', array_keys(self::$appData)));
 
-        return self::$appData[$bankID];
+        return self::$appData[ $bankID ];
     }
 }
