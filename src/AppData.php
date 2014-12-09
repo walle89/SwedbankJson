4a8b734c3d14bbbb59e953b18ad7116ec48829ee
Eric Wallmander <git@wallmander.nu>
2014-12-09 01:15:40 +0100

    Uppdatering av appversion (Swedbank, sparbanken samt respektive ungdoms- och företagsappar)

--- src/AppData.php
+++ src/AppData.php
@@ -15,12 +15,12 @@ namespace SwedbankJson;
 class AppData
 {
     private static $appData = [
-        'swedbank'              => [ 'appID' => 'HithYAGrzi8fu73j', 'useragent' => 'SwedbankMOBPrivateIOS/3.9.0_(iOS;_8.0.2)_Apple/iPhone5,2'       ],
-        'sparbanken'            => [ 'appID' => '9iZSu74jfDFaTdPd', 'useragent' => 'SavingbankMOBPrivateIOS/3.9.0_(iOS;_8.0.2)_Apple/iPhone5,2'     ],
-        'swedbank_ung'          => [ 'appID' => 'IV4Wrt2VZtyYjfpW', 'useragent' => 'SwedbankMOBYouthIOS/1.6.0_(iOS;_8.0.2)_Apple/iPhone5,2'         ],
-        'sparbanken_ung'        => [ 'appID' => 'BrGkZQR89rEbFwnj', 'useragent' => 'SavingbankMOBYouthIOS/1.6.0_(iOS;_8.0.2)_Apple/iPhone5,2'       ],
-        'swedbank_foretag'      => [ 'appID' => 'v0RVbFGKMXz7U4Eb', 'useragent' => 'SwedbankMOBCorporateIOS/1.5.0_(iOS;_8.0.2)_Apple/iPhone5,2'     ],
-        'sparbanken_foretag'    => [ 'appID' => 'JPf1VxiskNdFSclr', 'useragent' => 'SavingbankMOBCorporateIOS/1.5.0_(iOS;_8.0.2)_Apple/iPhone5,2'   ],
+        'swedbank'              => [ 'appID' => 'i1Pc8spRArqu9KAh', 'useragent' => 'SwedbankMOBPrivateIOS/4.0.0_(iOS;_8.1.1)_Apple/iPhone5,2'       ],
+        'sparbanken'            => [ 'appID' => 'NOhNNqhzTXXSoOdQ', 'useragent' => 'SavingbankMOBPrivateIOS/4.0.0_(iOS;_8.1.1)_Apple/iPhone5,2'     ],
+        'swedbank_ung'          => [ 'appID' => 'CXTbGZnvWjL4vVBr', 'useragent' => 'SwedbankMOBYouthIOS/1.7.0_(iOS;_8.1.1)_Apple/iPhone5,2'         ],
+        'sparbanken_ung'        => [ 'appID' => 'l8TOWVEHNtS1dCvd', 'useragent' => 'SavingbankMOBYouthIOS/1.7.0_(iOS;_8.1.1)_Apple/iPhone5,2'       ],
+        'swedbank_foretag'      => [ 'appID' => '4WXxfxvWDY5kd0eg', 'useragent' => 'SwedbankMOBCorporateIOS/1.6.0_(iOS;_8.1.1)_Apple/iPhone5,2'     ],
+        'sparbanken_foretag'    => [ 'appID' => 'qaSBwIdFFqRo48WD', 'useragent' => 'SavingbankMOBCorporateIOS/1.6.0_(iOS;_8.1.1)_Apple/iPhone5,2'   ],
     ];
 
     public static function bankAppId($bankID)
