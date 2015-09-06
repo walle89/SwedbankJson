c7269f9223790a89cd051192b1fc3d87a6db490a
Eric Wallmander <git@wallmander.nu>
2015-09-07 00:55:51 +0200

    Ingen behov av cookieJar sökväg

--- src/Auth/MobileBankID.php
+++ src/Auth/MobileBankID.php
@@ -31,17 +31,15 @@ class MobileBankID extends AbstractAuth
      * @param string|array      $bankApp    ID för vilken bank som ska anropas, eller array med appdata uppgifter.
      * @param int               $username   Personnummer för inlogging till internetbanken
      * @param bool              $debug      Sätt true för att göra felsökning, annars false eller null
-     * @param string            $ckfile     Sökväg till mapp där cookiejar kan sparas temporärt
      *
      * @throws \Exception
      * @throws \SwedbankJson\UserException
      */
-    public function __construct($bankApp, $username, $debug = false, $ckfile = './temp/')
+    public function __construct($bankApp, $username, $debug = false)
     {
         $this->setAppData((!is_array($bankApp)) ? AppData::bankAppId($bankApp) : $bankApp);
         $this->_username    = $username;
         $this->_debug       = (bool)$debug;
-        $this->_ckfile      = tempnam($ckfile, 'CURLCOOKIE');
         $this->setAuthorizationKey();
     }
 
