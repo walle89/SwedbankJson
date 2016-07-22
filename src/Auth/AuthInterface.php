7e7b6f2641bffbe1afc6158256cb25a682f5e3d8
Eric Wallmander <git@wallmander.nu>
2016-07-22 03:20:20 +0200

    Translation and reformating

--- src/Auth/AuthInterface.php
+++ src/Auth/AuthInterface.php
@@ -8,9 +8,9 @@ namespace SwedbankJson\Auth;
 interface AuthInterface
 {
     /**
-     * Inled inloggon
+     * Sign in
      *
-     * @return bool Om inloggingen lyckades eller ej
+     * @return bool True for successful sign in
      */
     public function login();
 }
